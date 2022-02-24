c9d19025c7fa33851688e10feaaa2484b253f6a0
Aditya Mulgundkar <adityamulgundkar.nmims@gmail.com>
2022-02-24 12:43:50 +0530

    MATLAB dataset reader updated

--- src_matlab/read_dataset.m
+++ src_matlab/read_dataset.m
@@ -23,7 +23,7 @@ lastParamUse = searchLastParamUse(end);
 lastParamTimestamp = DATASET.PARM.TimeUS(lastParamUse);
 
 % Select the dataset array you wish to work on (Baro, Gyro, EKF, ...)
-selectedArray = DATASET.BARO;
+selectedArray = DATASET.RATE;
 
 % Find closest timestamp in the BARO (Barometer) data array.
 % val stores the time value in us
@@ -31,9 +31,56 @@ selectedArray = DATASET.BARO;
 % timestamp in BARO, when compared to the input SERVO1_FUNCTION timestamp's
 % last call.
 [val, key] = min(abs(selectedArray.TimeUS-lastParamTimestamp));
-altitudeAtLastTimestamp=selectedArray.Alt(key);
+RATELastTimestamp=selectedArray.TimeUS(key);
 
-% The altitude array can now be observed from the selected key onwards.
+% TODO: Marker
+disp("RATELastTimestamp: " + RATELastTimestamp);
+
+% The selected array can now be observed from the selected key onwards.
 % i.e. this is the timestamp after which motor fault occurs.
-newAltitudeArray = selectedArray.Alt(key:end);
-plot(newAltitudeArray);
+
+% Create plots
+x = (selectedArray.TimeUS);
+t = tiledlayout(3,2);
+
+rateP = selectedArray.P;
+t1 = nexttile;
+plot(x, rateP);
+line([RATELastTimestamp RATELastTimestamp], ylim, 'Color',[1,0,0]);
+xlabel(t1,'Time in microseconds');
+ylabel(t1,'P');
+
+ratePDes = selectedArray.PDes;
+t2 = nexttile;
+plot(x, ratePDes);
+line([RATELastTimestamp RATELastTimestamp], ylim, 'Color',[1,0,0]);
+xlabel(t2,'Time in microseconds');
+ylabel(t2,'P Desired');
+
+rateY = selectedArray.Y;
+t3 = nexttile;
+plot(x, rateY);
+line([RATELastTimestamp RATELastTimestamp], ylim, 'Color',[1,0,0]);
+xlabel(t3,'Time in microseconds');
+ylabel(t3,'Y');
+
+rateYDes = selectedArray.YDes;
+t4 = nexttile;
+plot(x, rateYDes);
+line([RATELastTimestamp RATELastTimestamp], ylim, 'Color',[1,0,0]);
+xlabel(t4,'Time in microseconds');
+ylabel(t4,'Y Desired');
+
+rateR = selectedArray.R;
+t5 = nexttile;
+plot(x, rateR);
+line([RATELastTimestamp RATELastTimestamp], ylim, 'Color',[1,0,0]);
+xlabel(t5,'Time in microseconds');
+ylabel(t5,'R');
+
+rateRDes = selectedArray.RDes;
+t6 = nexttile;
+plot(x, rateRDes);
+line([RATELastTimestamp RATELastTimestamp], ylim, 'Color',[1,0,0]);
+xlabel(t6,'Time in microseconds');
+ylabel(t6,'R Desired');
